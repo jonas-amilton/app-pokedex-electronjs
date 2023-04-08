ed56d08a8fc9a6206fdbac1af1678cbf6c4fa873
Jonas Silva <90778653+jonas-amilton@users.noreply.github.com>
2023-04-08 01:41:23 -0200

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ app.on("ready", () => {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
   });
 
   mainWindow.loadURL(`file://${__dirname}/index.html`);
